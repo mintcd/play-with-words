aba62a8ee730b5f727a1c416b9145d2d17b65947
mintcd <dangminh2208@gmail.com>
2024-03-19 11:12:01 +0700

    Relocate

---
+++ test/test.py
@@ -0,0 +1,273 @@
+{'props': {'pageProps': {'currentWord': {'wordRoot': 'abandon',
+    'id': 5966,
+    'phonemic': 'É\x99Ë\x88bÃ¦ndÉ\x99n|É\x99Ë\x88bÃ¦ndÉ\x99n\r',
+    'bigId': 'a4456971-8ef0-4903-859d-5bca5ee0280d'
+      },
+   'senses': [
+        {'id': 'a4456971-8ef0-4903-859d-5bca5ee0280d',
+     'de': 'to leave someone, especially when they need you',
+     'ty': 'verb',
+     'ex': 'He abandoned his family.',
+     'use': '',
+     'sy': '',
+     'op': '',
+     're': '',
+     'tp': '',
+     'cl': ['abandon family', ' abandon friend', ' abandon love'
+          ],
+     'ImageSrc': 'https: //word-images.cdn-wordup.com/senses/a4456971-8ef0-4903-859d-5bca5ee0280d.webp?v=1',
+     'Tips': [
+            {'title': 'Emotional Weight',
+       'description': "Using 'abandon' can evoke strong feelings as it implies neglect or betrayal.",
+       'example': "Children felt abandoned when their parents didn't show up for the play.\r",
+       'imageUrl': 'https: //word-images.cdn-wordup.com/tips/qgw6hw3qQOwi20McnsvzZ8hajbA.webp'},
+              {'title': 'Not Temporary',
+       'description': "'Abandon' often suggests a permanent decision, not just leaving temporarily.",
+       'example': 'After years of struggle, he finally abandoned his dream of becoming an actor.',
+       'imageUrl': 'https: //word-images.cdn-wordup.com/tips/POPxHRf6uWAdjzsdDtWX5vETG4.webp'}]},
+                {'id': '18bb1e56-49c6-4723-a2f4-e86d315d45ca',
+     'de': 'to stop doing or cancel something',
+     'ty': 'verb',
+     'ex': 'They had to abandon the trip because of the bad weather.',
+     'use': '',
+     'sy': '',
+     'op': '',
+     're': '',
+     'tp': '',
+     'cl': ['abandon plan', ' abandon project', ' abandon idea'
+                  ],
+     'ImageSrc': 'https: //word-images.cdn-wordup.com/senses/18bb1e56-49c6-4723-a2f4-e86d315d45ca.webp?v=1',
+     'Tips': [
+                    {'title': 'Change of Plans',
+       'description': "'Abandon' implies a plan or activity is stopped before completion, usually for a specific reason.",
+       'example': 'Due to the storm, the team abandoned its attempt to reach the summit.\r',
+       'imageUrl': ''
+                    },
+                    {'title': 'Final Decision',
+       'description': "When you abandon something like a project, it means you're not going back to it.",
+       'example': 'The company abandoned its software update due to technical difficulties.',
+       'imageUrl': ''
+                    }
+                  ]
+                },
+                {'id': '06d89f54-a1b7-4c70-a53c-33a88cbbd5f7',
+     'de': 'to give up control absolutely; to subject oneself to something',
+     'ty': 'verb',
+     'ex': 'He abandoned himself to despair.',
+     'use': '',
+     'co': 'Literary',
+     'sy': '',
+     'op': '',
+     're': 'Disapproving',
+     'tp': '',
+     'cl': ['abandon oneself to despair',
+      ' abandon oneself to fate',
+      ' abandon oneself to pleasure'
+                  ],
+     'ImageSrc': 'https: //word-images.cdn-wordup.com/senses/06d89f54-a1b7-4c70-a53c-33a88cbbd5f7.webp?v=1',
+     'Tips': [
+                    {'title': 'Deep Involvement',
+       'description': "'Abandon' in this context means fully immersing oneself without resistance.",
+       'example': 'In grief, he abandoned himself to the flow of memories.\r',
+       'imageUrl': ''
+                    },
+                    {'title': 'Literary Flair',
+       'description': 'This usage is more common in literature, adding a dramatic or poetic tone.',
+       'example': "She abandoned herself to the mystery of the night's enchantment.",
+       'imageUrl': ''
+                    }
+                  ]
+                },
+                {'id': '93cfb1c6-e064-4a89-b4ed-dfa8aeae6123',
+     'de': 'complete lack of inhibition or restraint',
+     'ty': 'noun',
+     'ex': 'She danced with wild abandon.',
+     'use': '',
+     'sy': '',
+     'op': '',
+     're': 'Approving',
+     'tp': '',
+     'cl': ['dance with abandon', ' laugh with abandon', ' live with abandon'
+                  ],
+     'ImageSrc': 'https: //word-images.cdn-wordup.com/senses/93cfb1c6-e064-4a89-b4ed-dfa8aeae6123.webp?v=1',
+     'Tips': [
+                    {'title': 'Uninhibited Action',
+       'description': "As a noun, 'abandon' describes a carefree attitude, often positive and liberating.",
+       'example': 'The festival was a place of complete abandon, where everyone could be themselves.',
+       'imageUrl': ''
+                    }
+                  ]
+                },
+                {'id': 'aa140332-8059-4594-b947-b2468fc76bd2',
+     'de': 'to relinquish or forfeit a right or claim',
+     'do': 'legal',
+     'ty': 'verb',
+     'ex': 'He abandoned his right to the property.',
+     'use': '',
+     'sy': '',
+     'op': '',
+     're': 'Formal',
+     'tp': '',
+     'cl': ['abandon claim', ' abandon right', ' abandon property'
+                  ],
+     'ImageSrc': 'https: //word-images.cdn-wordup.com/senses/aa140332-8059-4594-b947-b2468fc76bd2.webp?v=1',
+     'Tips': [
+                    {'title': 'Legal Finality',
+       'description': "In legal terms, 'abandon' means giving up rights in a formal, often irreversible way.",
+       'example': 'The heir signed the document to abandon his inheritance.\r',
+       'imageUrl': ''
+                    },
+                    {'title': 'Specific Conditions',
+       'description': 'Legally abandoning something usually requires certain conditions or procedures.',
+       'example': 'Under the contract, the inventor could not abandon his claim to royalties.',
+       'imageUrl': ''
+                    }
+                  ]
+                }
+              ],
+   'comparisons': {'Abdicate': {'wordRoot': 'Abdicate',
+     'wordDefinition': 'Both abandon and abdicate refer to giving up or relinquishing something, but they are used in different contexts.\n\nAbdicate is used when someone gives up a position of power or authority, such as a king or queen deciding to give up their throne.\n\nFor example, if a monarch decides to abdicate, it means they are giving up their position as a ruler.\n\nAbandon is used when giving up something you were holding onto, while abdicate is used when giving up a position of power or authority.',
+     'wordImage': 'https: //word-images.cdn-wordup.com/senses/30fba510-7f85-4198-948f-ddbc9a779968.webp?v=1'},
+    'Desert': {'wordRoot': 'Desert',
+     'wordDefinition': 'Abandon and desert are both verbs that refer to leaving something or someone behind.\n\nDesert means to abandon, leave, or withdraw from someone or something out of a sense of necessity or reason, often due to unfavorable or harsh conditions.\n\nFor example, someone might desert a dangerous or difficult situation, or a soldier might desert their post.\n\nSo, abandonment implies a complete and deliberate disconnection, while desertion can be more circumstantial and understandable.',
+     'wordImage': 'https: //word-images.cdn-wordup.com/senses/06aaef94-ef72-4ed5-a32f-b18105d4659e.webp?v=1'},
+    'Disown': {'wordRoot': 'Disown',
+     'wordDefinition': "Disown means to reject or refuse to acknowledge something or someone as one's own, often due to a change in circumstances or personal beliefs.\n\nIt can be applicable in situations like disowning a family member due to their behavior, disowning a belief or ideology, or disowning a decision that was made in the past.",
+     'wordImage': 'https: //word-images.cdn-wordup.com/senses/c5fdba32-f3e9-4a1b-b662-f4079936b800.webp?v=1'},
+    'Forsake': {'wordRoot': 'Forsake',
+     'wordDefinition': 'Both abandon and forsake mean to leave behind, but the subtle difference is in the level of responsibility that one has towards the thing or person being left behind.\n\nAbandon implies leaving something or someone without regard for their welfare, whereas forsake implies leaving due to a sense of duty, a higher calling, or an unresolvable conflict.\n\nFor example, a parent may abandon their child by leaving them alone without food, shelter or care. But a soldier may forsake his family to serve his country, knowing that it is his duty even though it may cause emotional pain.\n\nAbandon describes a negative action that shows carelessness, neglect, or disregard towards something or someone, while forsake is often used to describe a positive action where a person chooses to leave behind something for a higher calling or a greater good.',
+     'wordImage': 'https: //word-images.cdn-wordup.com/senses/5a8f56ac-53f9-419e-bd70-e328484cf18c.webp?v=1'},
+    'Give up': {'wordRoot': 'Give up',
+     'wordDefinition': 'Give up means to cease trying, to relinquish or surrender. It implies a sense of defeat or resignation.\n\nGive up might be applicable when talking about a person who has given up on achieving a particular goal or a team that has given up on winning a game.\n\nAbandon implies a physical sense of leaving something behind, while give up implies a more emotional sense of surrender or defeat.',
+     'wordImage': 'https: //word-images.cdn-wordup.com/senses/fb0ae6c0-9951-453c-85d7-6c0c9624c2d5.webp?v=1'},
+    'Leave': {'wordRoot': 'Leave',
+     'wordDefinition': 'Leave means to depart from a place or situation.',
+     'wordImage': 'https: //word-images.cdn-wordup.com/senses/7512cdc6-ccba-4ee8-8c07-a14221c28da5.webp?v=1'},
+    'Neglect': {'wordRoot': 'Neglect',
+     'wordDefinition': 'Neglect means to fail to take care of something or someone, either intentionally or unintentionally. For example, neglecting to water a plant means not taking care of the plant, which could lead to its death.\n\nAbandon means leaving something or someone with no responsibility, while neglect means not taking good care of something or someone.',
+     'wordImage': 'https: //word-images.cdn-wordup.com/senses/01767325-0750-45ac-884b-339c782f6033.webp?v=1'},
+    'Relinquish': {'wordRoot': 'Relinquish',
+     'wordDefinition': 'Relinquish means to let go or release something, such as a possession, control or responsibility, willingly or under certain conditions.\n\nIt is more of a positive term and implies a sense of surrender or altruism. For instance, a person might relinquish their position or authority, a property or a right.\n\nAbandon is used when there is a sense of betrayal, rejection or disregard, while relinquish is used when there is a sense of sacrifice, generosity or compliance.',
+     'wordImage': 'https: //word-images.cdn-wordup.com/senses/733da940-4090-478d-9575-aa9dcd7bae82.webp?v=1'},
+    'Surrender': {'wordRoot': 'Surrender',
+     'wordDefinition': 'Surrender means to give up control or to give in to someone or something.\n\nFor example: A person may surrender in a fight or may surrender their weapons to the police.\n\nAbandon is used when you totally give up something, and surrender means giving up control of something or someone to someone else.',
+     'wordImage': 'https: //word-images.cdn-wordup.com/senses/a0619e96-d509-4b9d-bf56-12d4f832b98d.webp?v=1'}},
+   'videos': [
+                                    {'Text': ' But I also meet people who love what they do and couldn\'t imagine doing anything else. If you said, "Don\'t do this anymore," they\'d wonder what you\'re talking about. It isn\'t what they do, it\'s who they are. They say, "But this is me, you know. It would be foolish to abandon this,',
+     'Title': 'Bring on the learning...',
+     'VideoId': 'r9LelXa3U_I',
+     'Controls': 'start=214'
+                                    },
+                                    {'Text': " lessening the diet's effect over time and resulting in a quick reversal if the diet is abandoned. So while these diets may be alluring early on, they don't guarantee long-term benefits for your health and weight. A few simple guidelines, though, can help differentiate between",
+     'Title': 'How to spot a fad diet -...',
+     'VideoId': '8V15Z-yyiVg',
+     'Controls': 'start=111'
+                                    },
+                                    {'Text': " they're not what routine they can get very cold neck and get hypothermia which you can get to they do most of our penguins that come in as chicks which have been abandoned by their parents due to molting and so to help bolster the",
+     'Title': 'Endangered Penguins of...',
+     'VideoId': 'wwH3fygSNVU',
+     'Controls': 'start=102'
+                                    },
+                                    {'Text': " That applies both at the ocean's surface and deep below. Sailors have always known about currents, but their patterns weren't well known until recently. In fact, tracking abandoned ships was how scientists determined the shape",
+     'Title': 'Are ghost ships real? -...',
+     'VideoId': 'cURn__xlWXk',
+     'Controls': 'start=232'
+                                    },
+                                    {'Text': ' announced that you were taking a hiatus film abandoning your your musical career and you probably broke millions of hearts around the world including mine what what what what why whena would you do that',
+     'Title': 'A message of peace, hope...',
+     'VideoId': 'Ec2lto9dPo4',
+     'Controls': 'start=90'
+                                    },
+                                    {'Text': ' First, Psyche was told to sort a huge, messy pile of seeds in a single night. Just as she was abandoning hope, an ant colony took pity on her and helped with the work.',
+     'Title': 'The myth of Cupid and...',
+     'VideoId': 'Gjj_-CPxjCM',
+     'Controls': 'start=183'
+                                    },
+                                    {'Text': " either eat it or bury it secretly but the thief hasn't abandoned her life of crime spy squirrel has a new knot one with a camera theft is such an easy option around a",
+     'Title': 'Amazing Animal...',
+     'VideoId': 'GTPU6RnGj68',
+     'Controls': 'start=413'
+                                    },
+                                    {'Text': ' Right across the raid front, prey of all sizes are driven from their hiding places. Even wasps must abandon their homes when the ants arrive.',
+     'Title': 'Army Ants Rampage...',
+     'VideoId': 'JsfiUR0ZzLw',
+     'Controls': 'start=192'
+                                    },
+                                    {'Text': ' is over instead their strategy is to abandon their own desires and support their high status sibling creating a formidable band of brothers',
+     'Title': 'The Best Wingmen in the...',
+     'VideoId': '17Goh_nxslc',
+     'Controls': 'start=28'
+                                    },
+                                    {'Text': ' Hermit crabs protect themselves by hiding their soft bodies in the abandoned shells of other creatures.',
+     'Title': 'Hermit Crabs Use Litter...',
+     'VideoId': 'LuwQKJCO9UQ',
+     'Controls': 'start=0'
+                                    }
+                                  ],
+   'quotes': [
+                                    {'ImageSrc': 'https: //author.cdn-wordup.com/img-webp/thich-nhat-hanh.webp?v=1',
+     'Text': "In the past there were people who were not rich but contented with their living style, laughing and happy all day. But when the new rich people appear, people look at them and ask, 'why don't I have a life like that too, a beautiful house, car and garden,' and they abandon their values.",
+     'Title': 'Vietnamese Clergyman',
+     'Name': 'Thich Nhat Hanh'
+                                    },
+                                    {'ImageSrc': 'https: //author.cdn-wordup.com/img-webp/kirk-franklin.webp?v=1',
+     'Text': "When we go through being rejected and abandoned like I did as a kid, you have a lot of fear and anxiety issues that you didn't even know that's what it was defined as. You live your life a lot of times living with the ghost of fear.",
+     'Title': 'American Singer',
+     'Name': 'Kirk Franklin'
+                                    },
+                                    {'ImageSrc': 'https: //author.cdn-wordup.com/img-webp/hanya-yanagihara.webp?v=1',
+     'Text': "There comes a point when you're writing a novel when you're in it so deep that the life of the novel becomes more real to you than life itself. You have to write your way out of it; once you're there, it's too late to abandon.",
+     'Title': 'American Novelist',
+     'Name': 'Hanya Yanagihara'
+                                    },
+                                    {'ImageSrc': 'https: //author.cdn-wordup.com/img-webp/nastassja-kinski.webp?v=1',
+     'Text': "But when it really happens I'm very fascinated, I'm waiting for the moment, because the moment where life abandons you and death steps in, that moment must be fantastic, no?",
+     'Title': 'German Actress',
+     'Name': 'Nastassja Kinski'
+                                    },
+                                    {'ImageSrc': 'https: //author.cdn-wordup.com/img-webp/zach-braff.webp?v=1',
+     'Text': "When we shoot 'Scrubs' I spend every waking hour of my life in an abandoned and haunted hospital. All I can date there are ghosts and they tend to be horrible snugglers.",
+     'Title': 'American Actor',
+     'Name': 'Zach Braff'
+                                    },
+                                    {'ImageSrc': 'https: //author.cdn-wordup.com/img-webp/jonny-bairstow.webp?v=1',
+     'Text': 'My dad was an only child. His father raised him all but alone after his mother abandoned the two of them. He was only three years old.',
+     'Title': 'English Athlete',
+     'Name': 'Jonny Bairstow'
+                                    },
+                                    {'ImageSrc': 'https: //author.cdn-wordup.com/img-webp/jim-crace.webp?v=1',
+     'Text': 'When a book goes well, it abandons me. I am the most abandoned writer in the world.',
+     'Title': 'English Writer',
+     'Name': 'Jim Crace'
+                                    },
+                                    {'ImageSrc': 'https: //author.cdn-wordup.com/img-webp/joan-d-vinge.webp?v=1',
+     'Text': 'For every path you choose, there is another you must abandon, usually forever.',
+     'Title': 'American Author',
+     'Name': 'Joan D. Vinge'
+                                    },
+                                    {'ImageSrc': 'https: //author.cdn-wordup.com/img-webp/halle-berry.webp?v=1',
+     'Text': "My whole life I've had the fear that I was going to be abandoned.",
+     'Title': 'American Actress',
+     'Name': 'Halle Berry'
+                                    },
+                                    {'ImageSrc': 'https: //author.cdn-wordup.com/img-webp/james-q-wilson.webp?v=1',
+     'Text': 'Crime is the price society pays for abandoning character.',
+     'Title': 'American Politician',
+     'Name': 'James Q. Wilson'
+                                    }
+                                  ]
+                                },
+  '__N_SSG': True
+                              },
+ 'page': '/dictionary/[slug
+                              ]',
+ 'query': {'slug': 'abandon'
+                              },
+ 'buildId': 'GK0i0FA9vS6CU7i9Ve4ou',
+ 'isFallback': False,
+ 'gsp': True,
+ 'scriptLoader': [],
+ 'protips': {'Emotional Weight': " Using 'abandon' can evoke strong feelings as it implies neglect or betrayal. ",
+  'Not Temporary': " 'Abandon' often suggests a permanent decision, not just leaving temporarily. "
+                              }
+                            }
